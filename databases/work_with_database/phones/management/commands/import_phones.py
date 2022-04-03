18db8b5d742e37578cdb013abbc7a2ca61e98dd2
MakinFantasy <beingp0z1t1v3@gmail.com>
2022-04-03 19:34:39 +0300

    databases-1 completed

--- databases/work_with_database/phones/management/commands/import_phones.py
+++ databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,6 +1,7 @@
 import csv
 
 from django.core.management.base import BaseCommand
+from django.template.defaultfilters import slugify
 from phones.models import Phone
 
 
@@ -9,12 +10,18 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        with open('phones.csv', 'r') as csvfile:
+        with open('phones.csv', 'r') as file:
+            phones = list(csv.DictReader(file, delimiter=';'))
 
-            phone_reader = csv.reader(csvfile, delimiter=';')
-            # пропускаем заголовок
-            next(phone_reader)
+        for phone in phones:
+            id = list(phone.values())[0]
+            name = list(phone.values())[1]
+            image = list(phone.values())[2]
+            price = list(phone.values())[3]
+            release_date = list(phone.values())[4]
+            lte_exists = list(phone.values())[5]
+            slug = slugify(name)
 
-            for line in phone_reader:
-                # TODO: Добавьте сохранение модели
-                pass
+            phone = Phone(id=id, name=name, image=image, price=price, release_date=release_date,
+                          lte_exists=lte_exists, slug=slug)
+            phone.save()
